732869e6749bcf72e659e5c3a906b5af04444cfa
meriemelmediouny <meriemelmediouny1996@gmail.com>
2020-05-06 22:50:42 +0000

    added dossier to-do-liste

--- to-do-liste/todoliste.js
+++ to-do-liste/todoliste.js
@@ -1,120 +1,60 @@
-//SELECT ELEMENTS AND ASSIGN THEM TO VARS
-var newTask = document.querySelector('#new-task');
-var addTaskBtn = document.querySelector('#addTask');
-
-var toDoUl = document.querySelector(".todo-list ul");
-var completeUl =  document.querySelector(".complete-list ul");
-
-
-//CREATE FUNCTIONS
-
-//CREATING THE ACTUAL TASK LIST ITEM
-var createNewTask = function(task){
-  console.log("Creating task...");
-  
-  //SET UP THE NEW LIST ITEM
-  var listItem = document.createElement("li"); //<li>
-  var checkBox = document.createElement("input"); //checkbox
-  var label = document.createElement("label"); // <label>
-  
-  
-  //PULL THE INPUTED TEXT INTO LABEL
-  label.innerText = task;
-  
-  //ADD PROPERTIES
-  checkBox.type = "checkbox";
-  
-  //ADD ITEMS TO THE LI
-  listItem.appendChild(checkBox);
-  listItem.appendChild(label);
-  //EVERYTHING PUT TOGETHER
-  return listItem;  
-  
-};
-
-//ADD THE NEW TASK INTO ACTUAL INCOMPLETE LIST
-var addTask = function(){
-  console.log("Adding task...");
-  //FOR CLARITY, GRAB THE INPUTTED TEXT AND STORE IT IN A VAR
-  var listItem = createNewTask(newTask.value);
-  //ADD THE NEW LIST ITEM TO LIST
-  toDoUl.appendChild(listItem); 
-  //CLEAR THE INPUT
-  newTask.value="";
-  
-  //BIND THE NEW LIST ITEM TO THE INCOMPLETE LIST
-  bindIncompleteItems(listItem, completeTask);
-
-};
-
-var completeTask = function(){
-  
-  //GRAB THE CHECKBOX'S PARENT ELEMENT, THE LI IT'S IN
-  var listItem = this.parentNode;
-  
-  //CREATE AND INSERT THE DELETE BUTTON
-  var deleteBtn = document.createElement("button"); // <button>
-  deleteBtn.innerText ="Delete"; 
-  deleteBtn.className = "delete";
-  listItem.appendChild(deleteBtn);
-  
-  //SELECT THE CHECKBOX FROM THE COMPLETED CHECKBOX AND REMOVE IT
-  var checkBox = listItem.querySelector("input[type=checkbox]");
-  checkBox.remove();
-  
-  //PLACE IT INSIDE THE COMPLETED LIST
-  completeUl.appendChild(listItem); 
-  
-  //BIND THE NEW COMPLETED LIST
-  bindCompleteItems(listItem, deleteTask);
-  
-};
-
-//DELETE TASK FUNCTIONS
-var deleteTask = function(){
-  console.log("Deleting task...");
-  
-  var listItem = this.parentNode;
-  var ul = listItem.parentNode;
-  
-  ul.removeChild(listItem);
-  
-};
-
-//A FUNCTION THAT BINDS EACH OF THE ELEMENTS THE INCOMPLETE LIST
-
-var bindIncompleteItems = function(taskItem, checkBoxClick){  
-  console.log("Binding the incomplete list...");
-  
-  //BIND THE CHECKBOX TO A VAR
-  var checkBox = taskItem.querySelector("input[type=checkbox]");
-  
-  //SETUP EVENT LISTENER FOR THE CHECKBOX
-  checkBox.onchange = checkBoxClick;  
-}; 
-
-
-//A FUNCTIONM THAT BINDS EACH OF THE ELEMTS IN THE COMPLETE LIST
-var bindCompleteItems = function(taskItem, deleteButtonPress){
-  console.log("Binding the complete list...");
-  
-  //BIND THE DELETE BUTTON
-  var deleteButton = taskItem.querySelector(".delete");
-   
-  //WHEN THE DELETE BUTTIN IS PRESSED, RUN THE deleteTask function
-  deleteButton.onclick = deleteButtonPress;
-    
-};
-
-
-for(var i=0; i < toDoUl.children.length; i++) {
-  bindIncompleteItems(toDoUl.children[i], completeTask);
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-for(var i=0; i < completeUl.children.length; i++) {
-  bindCompleteItems(completeUl.children[i], deleteTask);
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+      if(confirm("voulez-vous vraiment supprimer?"))
+          {
+            var div = this.parentElement;
+            div.style.display = "none";      
+          }
+    }
 }
 
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
-addTaskBtn.addEventListener("click", addTask);
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      if(confirm("voulez-vous vraiment supprimer?"))
+          {
+            var div = this.parentElement;
+            div.style.display = "none";      
+          }
+    }
+  }
+}
